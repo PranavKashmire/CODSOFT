3e8fda7bc6aac2a796550784b2f60a87bb76c817
PranavKashmire <135645188+PranavKashmire@users.noreply.github.com>
2024-06-27 13:38:20 +0530

    Add files via upload

---
+++ task-1 - Personal Portflio/script.js
@@ -0,0 +1,54 @@
+// Navbar
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('active');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+
+// Scroll Reveal
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+
+sr.reveal(`.home-text, .home-img,
+            .about-img, .about-text,
+            .box, .s-box,
+            .btn, .connect-text,
+            .contact-box`, {
+    interval: 200
+})
+
+// abc
+document.addEventListener('DOMContentLoaded', function() {
+    const homeText = document.querySelector('.home-text');
+    const homeImg = document.querySelector('.home-img');
+
+    setTimeout(function() {
+        homeText.classList.add('show');
+        homeImg.classList.add('show');
+    }, 300); // Adjust the delay as needed
+});
+
+
+
